216e708fe6828cfb7b0a0ef9bcc157574f3aeb37
Esteban <esteban.horacio.acevedo.gmail.com>
2026-03-26 20:58:17 -0300

    Crea  modelo Pais y agrega campos a Cliente

--- src/cliente/admin.py
+++ src/cliente/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from cliente import models
+
+admin.site.register(models.Pais)
+admin.site.register(models.Cliente)
--- src/cliente/models.py
+++ src/cliente/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+    class Meta:
+        verbose_name = "país"
+        verbose_name_plural = "países"
+
+
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
+    nacimiento = models.DateField(null=True, blank=True)
+    pais_origen = models.ForeignKey(
+        Pais, on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    def __str__(self) -> str:
+        return f"{self.apellido.upper()}, {self.nombre}"
+
+    class Meta:
+        verbose_name_plural = "clientes"
