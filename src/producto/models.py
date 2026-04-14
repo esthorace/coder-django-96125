e7005c238915eab79cdf636a5af03ad4f7212475
Esteban <esteban.horacio.acevedo.gmail.com>
2026-04-14 18:45:41 -0300

    Crea modelo CategoriaProducto y lo registra en admin

--- src/producto/models.py
+++ src/producto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class CategoriaProducto(models.Model):
+    nombre = models.CharField(max_length=80, unique=True)
+    descripcion = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Categoría de Producto"
+        verbose_name_plural = "Categorías de Productos"
