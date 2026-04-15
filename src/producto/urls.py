6c8cdbd2bfbfe0685baed725a2c31a3e6c0d95a3
Esteban <esteban.horacio.acevedo.gmail.com>
2026-04-14 21:06:01 -0300

    Refactoriza a Vistas Basadas en Clases

--- src/producto/urls.py
+++ src/producto/urls.py
@@ -5,25 +5,46 @@ from producto import views
 app_name = "producto"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # path("", views.index, name="index"),
+    path("", views.CategoriaProductoList.as_view(), name="index"),
+    # path(
+    #     "categoriaproducto/create",
+    #     views.categoriaproducto_create,
+    #     name="categoriaproducto_create",
+    # ),
     path(
         "categoriaproducto/create",
-        views.categoriaproducto_create,
+        views.CategoriaProductoCreate.as_view(),
         name="categoriaproducto_create",
     ),
+    # path(
+    #     "categoriaproducto/detail/<int:pk>",
+    #     views.categoriaproducto_detail,
+    #     name="categoriaproducto_detail",
+    # ),
     path(
         "categoriaproducto/detail/<int:pk>",
-        views.categoriaproducto_detail,
+        views.CategoriaProductoDetail.as_view(),
         name="categoriaproducto_detail",
     ),
+    # path(
+    #     "categoriaproducto/update/<int:pk>",
+    #     views.categoriaproducto_update,
+    #     name="categoriaproducto_update",
+    # ),
     path(
         "categoriaproducto/update/<int:pk>",
-        views.categoriaproducto_update,
+        views.CategoriaProductoUpdate.as_view(),
         name="categoriaproducto_update",
     ),
+    # path(
+    #     "categoriaproducto/delete/<int:pk>",
+    #     views.categoriaproducto_delete,
+    #     name="categoriaproducto_delete",
+    # ),
     path(
         "categoriaproducto/delete/<int:pk>",
-        views.categoriaproducto_delete,
+        views.CategoriaProductoDelete.as_view(),
         name="categoriaproducto_delete",
     ),
 ]
--- src/producto/views.py
+++ src/producto/views.py
@@ -1,55 +1,98 @@
+from typing import Any
+
+from django.db.models.query import QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from producto.models import CategoriaProducto
 from producto.forms import CategoriaProductoForm
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    UpdateView,
+    DetailView,
+    ListView,
+)
 
 
-def index(request: HttpRequest):
-    consulta = request.GET.get("consulta")
-    if consulta:
-        query = CategoriaProducto.objects.filter(nombre__contains=consulta)
-    else:
-        query = CategoriaProducto.objects.all()
-    contexto = {"productos": query}
-    return render(request, "producto/index.html", contexto)
+# def index(request: HttpRequest):
+#     consulta = request.GET.get("consulta")
+#     if consulta:
+#         query = CategoriaProducto.objects.filter(nombre__contains=consulta)
+#     else:
+#         query = CategoriaProducto.objects.all()
+#     contexto = {"productos": query}
+#     return render(request, "producto/index.html", contexto)
 
 
-def categoriaproducto_create(request: HttpRequest) -> HttpResponse:
-    if request.method == "GET":
-        form = CategoriaProductoForm()
-        contexto = {"form": form}
-    if request.method == "POST":
-        form = CategoriaProductoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("producto:index")
-    return render(request, "producto/categoriaproducto_form.html", contexto)
+class CategoriaProductoList(ListView):
+    model = CategoriaProducto
+
+    def get_queryset(self):
+        consulta = self.request.GET.get("consulta")
+        if consulta:
+            query = CategoriaProducto.objects.filter(nombre__contains=consulta)
+        else:
+            query = CategoriaProducto.objects.all()
+        return query
 
 
-def categoriaproducto_detail(request: HttpRequest, pk: int) -> HttpResponse:
-    query = CategoriaProducto.objects.get(id=pk)
-    return render(
-        request, "producto/categoriaproducto_detail.html", {"categoria": query}
-    )
+# def categoriaproducto_create(request: HttpRequest) -> HttpResponse:
+#     if request.method == "GET":
+#         form = CategoriaProductoForm()
+#         contexto = {"form": form}
+#     if request.method == "POST":
+#         form = CategoriaProductoForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("producto:index")
+#     return render(request, "producto/categoriaproducto_form.html", contexto)
 
 
-def categoriaproducto_update(request: HttpRequest, pk: int) -> HttpResponse:
-    query = CategoriaProducto.objects.get(id=pk)
-    if request.method == "GET":
-        form = CategoriaProductoForm(instance=query)
-    if request.method == "POST":
-        form = CategoriaProductoForm(request.POST, instance=query)
-        if form.is_valid():
-            form.save()
-            return redirect("producto:index")
-    return render(request, "producto/categoriaproducto_form.html", {"form": form})
+class CategoriaProductoCreate(CreateView):
+    model = CategoriaProducto
+    form_class = CategoriaProductoForm
+    success_url = reverse_lazy("producto:index")
 
 
-def categoriaproducto_delete(request: HttpRequest, pk: int) -> HttpResponse:
-    query = CategoriaProducto.objects.get(id=pk)
-    if request.method == "POST":
-        query.delete()
-        return redirect("producto:index")
-    return render(
-        request, "producto/categoriaproducto_confirm_delete.html", {"categoria": query}
-    )
+# def categoriaproducto_detail(request: HttpRequest, pk: int) -> HttpResponse:
+#     query = CategoriaProducto.objects.get(id=pk)
+#     return render(
+#         request, "producto/categoriaproducto_detail.html", {"categoria": query}
+#     )
+
+
+class CategoriaProductoDetail(DetailView):
+    model = CategoriaProducto
+
+
+# def categoriaproducto_update(request: HttpRequest, pk: int) -> HttpResponse:
+#     query = CategoriaProducto.objects.get(id=pk)
+#     if request.method == "GET":
+#         form = CategoriaProductoForm(instance=query)
+#     if request.method == "POST":
+#         form = CategoriaProductoForm(request.POST, instance=query)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("producto:index")
+#     return render(request, "producto/categoriaproducto_form.html", {"form": form})
+
+
+class CategoriaProductoUpdate(UpdateView):
+    model = CategoriaProducto
+    form_class = CategoriaProductoForm
+    success_url = reverse_lazy("producto:index")
+
+
+# def categoriaproducto_delete(request: HttpRequest, pk: int) -> HttpResponse:
+#     query = CategoriaProducto.objects.get(id=pk)
+#     if request.method == "POST":
+#         query.delete()
+#         return redirect("producto:index")
+#     return render(
+#         request, "producto/categoriaproducto_confirm_delete.html", {"categoria": query}
+#     )
+
+class CategoriaProductoDelete(DeleteView):
+    model = CategoriaProducto
+    success_url = reverse_lazy("producto:index")
