2139e40d052904c78b82cef908f93839cb2d3c51
Esteban <esteban.horacio.acevedo.gmail.com>
2026-04-21 18:47:21 -0300

    Agrega modelo Producto

--- src/producto/admin.py
+++ src/producto/admin.py
@@ -3,3 +3,10 @@ from django.contrib import admin
 from producto import models
 
 admin.site.register(models.CategoriaProducto)
+
+
+@admin.register(models.Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("categoria", "nombre", "stock", "unidad_medida", "precio")
+    list_filter = ("categoria", "unidad_medida")
+    search_fields = ("nombre", "categoria__nombre")
--- src/producto/models.py
+++ src/producto/models.py
@@ -1,12 +1,37 @@
 from django.db import models
 
+
 class CategoriaProducto(models.Model):
     nombre = models.CharField(max_length=80, unique=True)
     descripcion = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.nombre
-    
+
     class Meta:
         verbose_name = "Categoría de Producto"
         verbose_name_plural = "Categorías de Productos"
+
+
+class Producto(models.Model):
+    categoria = models.ForeignKey(
+        CategoriaProducto,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="categoría",
+    )
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True, blank=True)
+    unidad_medida = models.CharField(max_length=50)
+    stock = models.FloatField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_actualizacion = models.DateTimeField(editable=False, auto_now=True)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.unidad_medida}) ${self.precio}"
+
+    class Meta:
+        verbos_name = "Producto"
+        verbos_name_plural = "Productos"
+        unique_together = ("categoria", "producto")
