7f8aa0b2a79def15ed7a94428f50e42530929e8d
Esteban <esteban.horacio.acevedo.gmail.com>
2026-04-23 19:44:59 -0300

    Crea model Venta

--- src/venta/admin.py
+++ src/venta/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from venta.models import Vendedor
+from venta.models import Vendedor, Venta
 
 admin.site.register(Vendedor)
+
+
+@admin.register(Venta)
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ("vendedor", "producto", "cantidad", "precio_total", "fecha_venta")
+    list_display_links = ("producto",)
+    search_fields = ("producto.nombre", "vendedor")
+    list_filter = ("vendedor",)
+    date_hierarchy = "fecha_venta"
--- src/venta/models.py
+++ src/venta/models.py
@@ -1,5 +1,8 @@
+from decimal import Decimal
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class Vendedor(models.Model):
@@ -15,3 +18,26 @@ class Vendedor(models.Model):
     class Meta:
         verbose_name = "Vendedor"
         verbose_name_plural = "Vendedores"
+
+
+class Venta(models.Model):
+    vendedor = models.ForeignKey(Vendedor, on_delete=models.PROTECT)
+    producto = models.ForeignKey("producto.Producto", on_delete=models.PROTECT)
+    cantidad = models.FloatField()
+    precio_total = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    fecha_venta = models.DateTimeField(default=timezone.now, editable=False)
+
+    class Meta:
+        ordering = ("-fecha_venta",)
+
+    def clean(self):
+        if self.cantidad > self.producto.stock:
+            raise ValidationError(
+                "La cantidad vendida no puede ser superior a la disponible"
+            )
+
+    def save(self, *args, **kwargs):
+        self.precio_total = self.producto.precio * Decimal(str(self.cantidad))
+        self.producto.stock -= self.cantidad
+        self.producto.save()
+        super().save(*args, **kwargs)
