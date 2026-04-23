5814e0468fff73c1b16ceb4ab1b87090c780630c
Esteban <esteban.horacio.acevedo.gmail.com>
2026-04-23 19:05:12 -0300

    Crea modelo Vendedor

--- src/venta/models.py
+++ src/venta/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Vendedor(models.Model):
+    usuario = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="vendedor"
+    )
+    celular = models.CharField(max_length=20, null=True, blank=True)
+    avatar = models.ImageField(upload_to="avatares", null=True, blank=True)
+
+    def __str__(self):
+        return self.usuario.username
+
+    class Meta:
+        verbose_name = "Vendedor"
+        verbose_name_plural = "Vendedores"
